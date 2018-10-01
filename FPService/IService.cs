c68618f9aeed8a7561d1f23bf2942568f6befa2d
Sultonazar Mamadazizov <sultonazar@outlook.com>
2018-10-01 11:14:28 +0500

    web congif multi site binding enabled

---
+++ FPService/IService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FPService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped,
+            Method = "GET", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "/hello")]
+        string Hello();
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped,
+            Method = "POST", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "z/identify_user")]
+        int Identify(string print, string server, string user,
+             string password, string db, string query, string idFieldName,
+             string printFieldName, int threadsCount);
+    }   
+}
